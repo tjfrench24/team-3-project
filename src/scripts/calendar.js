343592a88716431c1e0232f9e84a6f3323f3a941
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-11-14 21:35:52 -0500

    Break up javascript

---
+++ src/scripts/calendar.js
@@ -0,0 +1,53 @@
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  export function buildCalendar(date) {
+    const numCols = 7,
+      numRows = 5,
+      theGrid = document.getElementById("theGrid");
+    const monthYearDisplay = document.getElementById("monthYearDisplay");
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    monthYearDisplay.textContent = `${monthNames[month]} ${year}`;
+  
+    while (theGrid.firstChild) {
+      theGrid.removeChild(theGrid.firstChild);
+    }
+  
+    const firstDay = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const totalCells = numCols * numRows;
+  
+    for (let i = 0; i < firstDay; i++) {
+      const emptySquare = document.createElement("div");
+      emptySquare.classList.add("grid-item");
+      theGrid.appendChild(emptySquare);
+    }
+  
+    for (let day = 1; day <= daysInMonth; day++) {
+      const square = document.createElement("div");
+      square.textContent = day;
+      square.classList.add("grid-item", "white");
+      theGrid.appendChild(square);
+    }
+  
+    const remainingCells = totalCells - firstDay - daysInMonth;
+    for (let i = 0; i < remainingCells; i++) {
+      const emptySquare = document.createElement("div");
+      emptySquare.classList.add("grid-item");
+      theGrid.appendChild(emptySquare);
+    }
+  }
+  
