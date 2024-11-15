87d76d3f38e8f1479833838899b97898ab648c9a
lizhalp <119753562+lizhalp@users.noreply.github.com>
2024-11-15 17:43:29 -0500

    Add weekdays to calendar, modify styling

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -12,7 +12,8 @@ const monthNames = [
     "November",
     "December",
   ];
-  
+const weekdayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   export function buildCalendar(date) {
     const numCols = 7,
       numRows = 5,
@@ -29,25 +30,28 @@ const monthNames = [
     const firstDay = new Date(year, month, 1).getDay();
     const daysInMonth = new Date(year, month + 1, 0).getDate();
     const totalCells = numCols * numRows;
-  
+    for (let i = 0; i < numCols; i++) {
+        const weekday = document.createElement("span");
+        weekday.textContent = weekdayNames[i];
+        theGrid.appendChild(weekday);
+      }
     for (let i = 0; i < firstDay; i++) {
       const emptySquare = document.createElement("div");
-      emptySquare.classList.add("grid-item");
+      emptySquare.classList.add("grid-item", "empty");
       theGrid.appendChild(emptySquare);
     }
   
     for (let day = 1; day <= daysInMonth; day++) {
       const square = document.createElement("div");
       square.textContent = day;
-      square.classList.add("grid-item", "white");
+      square.classList.add("grid-item");
       theGrid.appendChild(square);
     }
   
     const remainingCells = totalCells - firstDay - daysInMonth;
     for (let i = 0; i < remainingCells; i++) {
       const emptySquare = document.createElement("div");
-      emptySquare.classList.add("grid-item");
+      emptySquare.classList.add("grid-item", "empty");
       theGrid.appendChild(emptySquare);
     }
   }
-  
