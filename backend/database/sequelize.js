635c7436b049a8375d0cf8a7c75dc31183b24438
tjfrench24 <146013400+tjfrench24@users.noreply.github.com>
2024-12-10 20:55:44 -0500

    fix sequelize import

--- backend/database/sequelize.js
+++ backend/database/sequelize.js
@@ -1,9 +1,9 @@
-//const { Sequelize } = require('sequelize');
 import { Sequelize } from "sequelize";
+
 // create sequelize instance
 const sequelize = new Sequelize({ 
-  dialect: 'sqlite',     // database is sqlite 
-  storage: './database.sqlite', // shows where to create the database file 
+  dialect: 'sqlite',      
+  storage: './database.sqlite', 
 });
 
 export default sequelize;  
